1b2e5cb4a2444cdfb7d3f24b4fe971fe250f7198
luke <lukeallisterfrank@gmail.com>
2020-10-08 17:34:16 +0200

    test first addition

---
+++ src/components/Test/NextButton.js
@@ -0,0 +1,9 @@
+import React  from 'react'
+
+const NextButton = ({nextButtonClicked}) => {
+	return(
+			<button onClick={nextButtonClicked} >Next</button>
+		)
+}
+
+export default NextButton
--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -1,11 +1,29 @@
 import React,{Component} from 'react'
+//import Anime from './Anime'
 
+const animeList = ['AOT', 'DBZ', 'Naruto']
 
 class Test extends Component {
+constructor() {
+  super()
+  this.state= {
+  	index: 0
+  }
+}
+buttonClicked() {
+	console.log('button was clicked')
+	this.setState((prevState) => {
+		return{ index: prevState.index + 1 }
+	})
+}
+
 	render() {
 		return(
 			<div>
-			
+				<p> {animeList[this.state.index]} </p>
+				<button onClick= {() => this.buttonClicked()}>
+					Next
+				</button>	
 			</div>
 			)
 	}
